053baf1d91b9fddb454db2e02f54627c7061001f
marvin <marvin@gels.dev>
2023-11-09 21:04:46 +0000

    fix: baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
         }
     },
     ssr: false,
+    baseURL: '/sakai-nuxt/', // baseURL: '/<repository>/'
     modules: ['nuxt-primevue'],
     primevue: {
         options: { ripple: true },
